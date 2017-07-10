e1a3b1c904e415a47f8ba55beffde90d382f7f30
EvgenyErmolovich <ermolovich.evgeny@gmail.com>
2017-07-10 23:10:45 +0300

    Code

---
+++ LogicNOD/NOD.cs
@@ -0,0 +1,110 @@
+﻿using System;
+namespace LogicNOD
+{
+	public static class NOD
+	{
+		/// <summary>
+		/// Euclid algorithm to find NOD.
+		/// </summary>
+		/// <returns>The NOD.</returns>
+		/// <param name="a">The alpha component.</param>
+		/// <param name="b">The blue component.</param>
+		public static long Euclid(long a, long b)
+		{
+			Check(a, b);
+			return EuclidLogic(a, b);
+		}
+		public static long Euclid(long a, long b, long c)
+		{
+			Check(a, b, c);
+			return EuclidLogic(EuclidLogic(a, b), c);
+		}
+		public static long Euclid(params long[] numbers)
+		{
+			Check(numbers);
+			for (int i = 0; i < numbers.Length - 1; i++)
+			{
+				numbers[i + 1] = EuclidLogic(numbers[i], numbers[i + 1]);
+			}
+			return numbers[numbers.Length - 1];
+		}
+		/// <summary>
+		/// Stein algorithm to find NOD.
+		/// </summary>
+		/// <returns>The NOD.</returns>
+		/// <param name="a">The alpha component.</param>
+		/// <param name="b">The blue component.</param>
+		public static long Stein(long a, long b)
+		{
+			Check(a, b);
+			return SteinLogic(a, b);
+		}
+		public static long Stein(long a, long b, long c)
+		{
+			Check(a, b, c);
+			return SteinLogic(SteinLogic(a, b), c);
+		}
+		public static long Stein(params long[] numbers)
+		{
+			Check(numbers);
+			for (int i = 0; i < numbers.Length - 1; i++)
+			{
+				numbers[i + 1] = SteinLogic(numbers[i], numbers[i + 1]);
+			}
+			return numbers[numbers.Length - 1];
+		}
+		/// <summary>
+		/// Check the specified numbers.
+		/// </summary>
+		/// <returns>The check.</returns>
+		/// <param name="numbers">Numbers.</param>
+		private static void Check(params long[] numbers)
+		{
+			if (numbers.Length < 2)
+				throw new ArgumentException("Should be >= 2 parameters");
+			for (int i = 0; i < numbers.Length; i++)
+				if (numbers[i] < 0) throw new ArgumentOutOfRangeException("Cannot be negative!");
+		}
+		/// <summary>
+		/// Euclids logic.
+		/// </summary>
+		/// <returns>The NOD.</returns>
+		/// <param name="a">The alpha component.</param>
+		/// <param name="b">The blue component.</param>
+		private static long EuclidLogic(long a, long b)
+		{
+			while (a > 0 && b > 0)
+				if (a >= b)
+					a %= b;
+				else
+					b %= a;
+			return a | b;
+		}
+		/// <summary>
+		/// Steins logic.
+		/// </summary>
+		/// <returns>The NOD.</returns>
+		/// <param name="a">The alpha component.</param>
+		/// <param name="b">The blue component.</param>
+		private static long SteinLogic(long a, long b)
+		{
+			if (a == 0) return b;
+			if (b == 0) return a;
+			if (a == b) return a;
+
+			bool val1IsEven = (a & 1u) == 0;
+			bool val2IsEven = (b & 1u) == 0;
+
+			if (val1IsEven && val2IsEven)
+				return Stein(a >> 1, b >> 1) << 1;
+			else if (val1IsEven)
+				return Stein(a >> 1, b);
+			else if (val2IsEven)
+				return Stein(a, b >> 1);
+			else if (a > b)
+				return Stein((a - b) >> 1, b);
+			else
+				return Stein(a, (b - a) >> 1);
+		}
+    }
+}
---
+++ NOD.NUnit.Tests/NOD.Tests.cs
@@ -0,0 +1,105 @@
+﻿using NUnit.Framework;
+using System;
+namespace LogicNOD.NUnit.Tests
+{
+	[TestFixture()]
+	public class NODTests
+	{
+		[TestCase(1200, 60, ExpectedResult = 60)]
+		[TestCase(116150, 232704, ExpectedResult = 202)]
+		[TestCase(15, 4, ExpectedResult = 1)]
+		public long Euclid_2Args(long a, long b)
+		{
+			return NOD.Euclid(a, b);
+		}
+
+		[TestCase(5, -10)]
+		[TestCase(-5, 10)]
+		public void Euclid_2Args_ThrowsArgumentOutOfRangeException(long a, long b)
+		{
+			Assert.Throws<ArgumentOutOfRangeException>(() => NOD.Euclid(a, b));
+		}
+
+		[TestCase(1200, 60, 120, ExpectedResult = 60)]
+		[TestCase(116150, 232704, 404, ExpectedResult = 202)]
+		[TestCase(15, 4, 315, ExpectedResult = 1)]
+		public long Euclid_3Args(long a, long b, long c)
+		{
+			return NOD.Euclid(a, b);
+		}
+
+		[TestCase(8, -15, 888)]
+		[TestCase(-8, 15, 888)]
+		[TestCase(8, 15, -888)]
+		public void Euclid_3Args_ThrowsArgumentOutOfRangeException(long a, long b, long c)
+		{
+			Assert.Throws<ArgumentOutOfRangeException>(() => NOD.Euclid(a, b, c));
+		}
+
+		[TestCase(2, 4, 8, 16, 32, ExpectedResult = 2)]
+		[TestCase(116150, 232704, 404, 8080, 202, ExpectedResult = 202)]
+		[TestCase(15, 4, 315, 78, 56565, ExpectedResult = 1)]
+		public long Euclid_ManyArgs(long a, long b, long c, long d, long e)
+		{
+			return NOD.Euclid(a, b, c, d, e);
+		}
+
+		[TestCase(8, -15, 888, 12, 23)]
+		[TestCase(-8, 15, 888, 12, 23)]
+		[TestCase(8, 15, -888, 12, 23)]
+		[TestCase(8, 15, 888, -12, 23)]
+		[TestCase(8, 15, 888, 12, -23)]
+		public void Euclid_ManyArgs_ThrowsArgumentOutOfRangeException(long a, long b, long c, long d, long e)
+		{
+			Assert.Throws<ArgumentOutOfRangeException>(() => NOD.Euclid(a, b, c, d, e));
+		}
+
+		[TestCase(1200, 60, ExpectedResult = 60)]
+		[TestCase(116150, 232704, ExpectedResult = 202)]
+		[TestCase(15, 4, ExpectedResult = 1)]
+		public long Stein_2Args(long a, long b)
+		{
+
+			return NOD.Stein(a, b);
+		}
+		[TestCase(8, -15)]
+		[TestCase(-8, 15)]
+		public void Stein_2Args_ThrowsArgumentOutOfRangeException(long a, long b)
+		{
+			Assert.Throws<ArgumentOutOfRangeException>(() => NOD.Stein(a, b));
+		}
+
+		[TestCase(1200, 60, 120, ExpectedResult = 60)]
+		[TestCase(116150, 232704, 404, ExpectedResult = 202)]
+		[TestCase(15, 4, 315, ExpectedResult = 1)]
+		public long Stein_3Args(long a, long b, long c)
+		{
+			return NOD.Stein(a, b);
+		}
+		[TestCase(8, -15, 888)]
+		[TestCase(-8, 15, 888)]
+		[TestCase(8, 15, -888)]
+		public void Stein_3Args_ThrowsArgumentOutOfRangeException(long a, long b, long c)
+		{
+			Assert.Throws<ArgumentOutOfRangeException>(() => NOD.Stein(a, b, c));
+		}
+
+		[TestCase(2, 4, 8, 16, 32, ExpectedResult = 2)]
+		[TestCase(116150, 232704, 404, 8080, 202, ExpectedResult = 202)]
+		[TestCase(15, 4, 315, 78, 56565, ExpectedResult = 1)]
+		public long Stein_ManyArgs(long a, long b, long c, long d, long e)
+		{
+			return NOD.Stein(a, b, c, d, e);
+		}
+
+		[TestCase(8, -15, 888, 12, 23)]
+		[TestCase(-8, 15, 888, 12, 23)]
+		[TestCase(8, 15, -888, 12, 23)]
+		[TestCase(8, 15, 888, -12, 23)]
+		[TestCase(8, 15, 888, 12, -23)]
+		public void Stein_ManyArgs_ThrowsArgumentOutOfRangeException(long a, long b, long c, long d, long e)
+		{
+			Assert.Throws<ArgumentOutOfRangeException>(() => NOD.Stein(a, b, c, d, e));
+		}
+	}
+}
